67716ae4a821563ae70d6caaa3932ffb141044b8
App Academy Student <student@appacademy.io>
2014-02-21 14:19:29 -0500

    Adds CONSTANTS file and CSS changes before changing database type.

---
+++ config/initializers/constants.rb
@@ -0,0 +1,46 @@
+PROFILE_DETAILS = {
+  body_type: BODY_TYPE,
+  drugs: DRUGS,
+  religion: RELIGION,
+  smokes: SMOKES,
+  sexual_orientation: SEXUAL_ORIENTATION}
+
+SEXUAL_ORIENTATION = {
+  1: "Straight",
+  2: "Gay",
+  3: "Bisexual",
+  4: "Anything I can get"
+}
+
+BODY_TYPE = {
+  1: "Swole",
+  2: "Average",
+  3: "Curvy",
+  4: "Full-figured",
+  5: "Used up"}
+
+DRUGS = {
+  1: "I should probably get some help",
+  2: "Socially like at music festivals",
+  3: "Rarely",
+  4: "Straight-edge"
+}
+
+RELIGION = {
+  1: "Atheism",
+  2: "Christian",
+  3: "Catholic",
+  4: "Hinduism",
+  5: "Muslim",
+  6: "Buddhism",
+  7: "Judaism"
+}
+
+SMOKES = {
+  1: "Yes",
+  2: "No",
+  3: "When drinking",
+  4: "Not cigarettes"
+}
+
+
