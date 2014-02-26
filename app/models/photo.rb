083d2cf193a1bb332fb4936840d344bf7c917815
App Academy Student <raman.sycheuski@gmail.com>
2014-02-26 16:25:07 -0500

    Commit before working with keymasteer II

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,8 +1,8 @@
 class Photo < ActiveRecord::Base
 
   attr_accessible :file, :profile_id
-
-  has_attached_file :file, :styles => {:featured => "840x>", :browse => "460x460#", :avatar => "200x200#", :thumb => "60x60#"}
+  # default_url: ActionController::Base.helpers.asset_path('blank_person_:style.png'),
+  has_attached_file :file, default_url: ActionController::Base.helpers.asset_path("img_placeholder.png"), :styles => {:featured => "840x>", :browse => "460x460#", :avatar => "200x200#", :thumb => "60x60#"}
   #doubled for retina display.
   validates_attachment_content_type :file, :content_type => /\Aimage\/.*\Z/
 
