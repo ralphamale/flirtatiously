2870594c47704f4989586db110d4781329346696
App Academy Student <student@appacademy.io>
2014-02-27 21:09:18 -0500

    Git Commit before leaving office before Final Day

---
+++ untitled 2.js
@@ -0,0 +1,6 @@
+
+<li><a href="#" class="message">Message</a></li>
+
+<li><a class="sec-nav-button change-settings" href="#" id="like-button">Notifications</a></li>
+
+<li><a class="sec-nav-button change-settings" href="#" id="like-button">Notifications</a></li>
