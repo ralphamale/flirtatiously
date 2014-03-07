c538b920ffd9ea6908d08c7de62d30e62f910714
Ralph Nguyen <ralph.nguyen@gmail.com>
2014-03-07 18:58:37 -0500

    Adds model tests for user filter.

---
+++ remaining tests
@@ -0,0 +1,21 @@
+AR
+it "should only allow one answer choice ID per user"
+
+Notifications
+pending { should ensure_inclusion_of(:is_read).in_array([true,false]) }
+
+Profile
+it "should not find previously matched profiles"
+it "should correctly apply filters"
+it "should suggest six profiles that have not been rated before"
+
+Rating
+pending { should ensure_inclusion_of(:likes).in_array([true,false]) }
+it "should correctly tell you if the rating is mutual or not"
+
+
+  User
+
+  it "should calculate match percentages correctly"
+  it "should find new notifications correctly"
+  it "should not confuse new notifications for old notifications"
--- spec/models/user_filter_spec.rb
+++ spec/models/user_filter_spec.rb
@@ -16,16 +16,76 @@
 require 'spec_helper'
 
 describe UserFilter do
+
+  before {
+    user = FactoryGirl.create(:user)
+    UserFilter.create!({user_id: user.id,
+      beg_age: 20,
+      end_age: 50,
+      sex: "M",
+      distance: 500,
+      sexual_orientation: 0})
+  }
+
+  subject(:user_filter) { UserFilter.last }
+
+  it "should be valid if all attributes are valid" do
+    expect(user_filter).to be_valid
+  end
+
   it { should validate_presence_of(:user) }
 
-  pending "Beg age should be 0 < x < 100 and < end_age"
-  pending "End age should be 0 < x < 100 and < end_age"
-  pending "Distance should be greater than 0 and be able to be nil"
-  pending "Sex is 'M' or 'F' with allow nil"
-  pending "Sexual Orientation should be 0..2 with nil okay"
+  it "should only allow beg_age and end_age to be 0 < x < 100" do
+    user_filter.beg_age = -1
+    expect(user_filter).to_not be_valid
+
+    user_filter.beg_age = 101
+    expect(user_filter).to_not be_valid
+
+    user_filter.beg_age = 40
+    expect(user_filter).to be_valid
+
+    user_filter.end_age = 101
+    expect(user_filter).to_not be_valid
+    user_filter.end_age = 60
+    expect(user_filter).to be_valid
+  end
+
+  it "should not let beg_age be higher than end_age" do
+    user_filter.beg_age = 60
+    expect(user_filter).to_not be_valid
+  end
+
+  it "should only allow distance greater than 0 or nil" do
+    user_filter.distance = 0
+    expect(user_filter).to_not be_valid
+
+    user_filter.distance = nil
+    expect(user_filter).to be_valid
+  end
+
+  it "should only allow sex to be 'M' or 'F' or nil" do
+    user_filter.sex = "F"
+    expect(user_filter).to be_valid
+
+    user_filter.sex = nil
+    expect(user_filter).to be_valid
+
+    user_filter.sex = "Meow"
+    expect(user_filter).to_not be_valid
+  end
+
+  it "should only allow sexual orientation to be 0..2 with nil okay" do
+    user_filter.sexual_orientation = 2
+    expect(user_filter).to be_valid
+
+    user_filter.sexual_orientation = nil
+    expect(user_filter).to be_valid
+
+    user_filter.sexual_orientation = 500
+    expect(user_filter).to_not be_valid
+  end
 
   it { should belong_to(:user) }
-
-  it "should convert blank attributes to blank"
 
 end
